c3dee7bc568138c40ea414edfb00388ff7eb02ef
wjeb <kirill.bugvin@gmail.com>
2016-12-13 20:58:28 +0200

    1
    1

--- src/android/com/c2c/CameraActivity.java
+++ src/android/com/c2c/CameraActivity.java
@@ -307,7 +307,7 @@ public class CameraActivity extends Fragment {
 				
 				//eventListener.onPictureTaken("alert:Display: "+width+"x"+height+"\nPicture: "+portraitPicture.getWidth()+"x"+portraitPicture.getHeight()+"\nResult: "+picWidth+"x"+picHeight);
 				
-				int widthPercent = 22;
+				int widthPercent = 30;
 				int boxSideSize = (int) Math.round(picWidth / 100 * widthPercent);
 				
 				leftMargin = leftMargin + ((picWidth - boxSideSize) / 2);
