169dad753ff99521edcde879ffc1e2cd0884d333
wjeb <kirill.bugvin@gmail.com>
2018-10-06 22:38:04 +0300

    1
    1

--- src/android/com/c2c/CameraPreview.java
+++ src/android/com/c2c/CameraPreview.java
@@ -69,6 +69,8 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
 			return false;
 		}
 		
+		alertView("StartCamera");
+		
 		fragment = new CameraActivity();
 		fragment.setEventListener(this);
 		
@@ -212,5 +214,20 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
 		return true;
 		
 	}
+	
+	private void alertView( String message ) {
+	 AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+	 dialog.setTitle( "Hello" )
+		   .setIcon(R.drawable.ic_launcher)
+		   .setMessage(message)
+	//     .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+	//      public void onClick(DialogInterface dialoginterface, int i) {
+	//          dialoginterface.cancel();   
+	//          }})
+		  .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialoginterface, int i) {   
+			}               
+			}).show();
+	 }
 	
 }
