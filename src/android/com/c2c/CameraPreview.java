c9a54e891ac87d91de21aa5b9668e84aadff94e2
wjeb <kirill.bugvin@gmail.com>
2019-02-23 16:59:01 +0200

    1
    1

--- src/android/com/c2c/CameraPreview.java
+++ src/android/com/c2c/CameraPreview.java
@@ -120,7 +120,7 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
 		
         //@Override
         //public void possibleResultPoints(List<ResultPoint> resultPoints) {
-		//	
+		//	 
 		//}
 		
     };
