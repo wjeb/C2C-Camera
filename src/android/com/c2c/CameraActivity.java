9c15feb984538169b42dc067333d59fbaf42634d
wjeb <kirill.bugvin@gmail.com>
2019-02-24 19:40:24 +0200

    1
    1

--- src/android/com/c2c/CameraActivity.java
+++ src/android/com/c2c/CameraActivity.java
@@ -861,14 +861,10 @@ class Preview extends RelativeLayout implements SurfaceHolder.Callback {
 				
 				final Matrix matrix = new Matrix();
 					
-					matrix.preScale(-1.0f, 1.0f);
-					
-					/*
 					if(fragment.cameraCurrentlyLocked == Camera.CameraInfo.CAMERA_FACING_FRONT) {
 						Log.d(TAG, "LOL: mirror y axis");
 						matrix.preScale(-1.0f, 1.0f);
 					}
-					*/
 					
 				//matrix.postRotate(fragment.mPreview.getDisplayOrientation());
 				
